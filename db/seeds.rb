6a5d0fb510110055f75f72971189c3ee372eff46
BlakeMcD <blakemcd@gmail.com>
2021-11-08 10:47:37 +1100

    changed admin email

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 
 #User
-User.create(username: 'Blakeezy', password: '12345', admin: true, first_name: 'Blake', last_name: 'McDeezy', email: 'blake@mcdeezy.com', phone: '0491 570 159', dob: 19880219)
+User.create(username: 'Blakeezy', password: '12345', admin: true, first_name: 'Blake', last_name: 'McDeezy', email: 'blakemcd@gmail.com', phone: '0491 570 159', dob: 19880219)
 User.create(username: 'Peteezy', password: '12345', admin: false, first_name: 'Peter', last_name: 'Parker', email: 'peterparker@gmail.com', phone: '0491 570 156', dob: 20010810)
 User.create(username: 'Toneezy', password: '12345', admin: false, first_name: 'Tony', last_name: 'Stark', email: 'tonystark@yahoo.com', phone: '0491 570 006', dob: 19700529)
 User.create(username: 'Steveezy', password: '12345', admin: false, first_name: 'Steve', last_name: 'Rogers', email: 'steverogers@hotmail.com', phone: '0491 570 313', dob: 19180604)
